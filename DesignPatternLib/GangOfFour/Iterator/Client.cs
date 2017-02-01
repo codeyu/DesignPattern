3b257627cf9280d49c0caed321376ee5dee9b4b3
codeyu <codeyu@gmail.com>
2017-02-01 14:00:36 +0800

    Iterator Design Pattern

---
+++ DesignPatternLib/GangOfFour/Iterator/Client.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace DesignPatternLib.Iterator
+{
+    public class Client
+    {
+        public static void Test()
+        {
+            // Create and item collection
+            var collection = new ItemCollection<Item>
+              {
+                new Item{ Name = "Item 0"},
+                new Item{ Name = "Item 1"},
+                new Item{ Name = "Item 2"},
+                new Item{ Name = "Item 3"},
+                new Item{ Name = "Item 4"},
+                new Item{ Name = "Item 5"},
+                new Item{ Name = "Item 6"},
+                new Item{ Name = "Item 7"},
+                new Item{ Name = "Item 8"}
+              };
+            Console.WriteLine("Iterate front to back");
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine("\nIterate back to front");
+            foreach (var item in collection.BackToFront)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine();
+            // Iterate given range and step over even ones
+            Console.WriteLine("\nIterate range (1-7) in steps of 2");
+            foreach (var item in collection.FromToStep(1, 7, 2))
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatternLib/GangOfFour/Iterator/Item.cs
@@ -0,0 +1,11 @@
+namespace DesignPatternLib.Iterator
+{
+    /// <summary>
+    /// The collection item
+    /// </summary>
+    class Item
+    {
+        // Gets or sets item name
+        public string Name { get; set; }
+    }
+}
